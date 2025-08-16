93dffcc84286aa77ad98ab264516e8ceb7d884d1
mkdodos <mkdodos@gmail.com>
2025-08-16 16:56:30 +0800

    done

--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -18,100 +18,99 @@ export default function index() {
 
   const [rows, setRows] = useState([]);
 
-  // getData();
+  getData();
 
   // 資料
-  const data = [
-    {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-    {
-      title: "Foreign Market Insight",
-      date: "2025-08-06",
-      id: "21HXfUsvcuCJr8tD5l1O",
-      content:
-        "翻開2張出口卡\n選擇1張換掉現在的出口卡\n其餘卡丟棄\n然後,你可以做一次出售到國外的動作",
-      class: "資本家",
-    },
-    {
-      title: "Foreign Market Insight",
-      date: "2025-08-06",
-      id: "21HXfUsvcuCJr8tD5l1O",
-      content:
-        "翻開2張出口卡\n選擇1張換掉現在的出口卡\n其餘卡丟棄\n然後,你可以做一次出售到國外的動作",
-      class: "資本家",
-    },
-    {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-    {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-    {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-    {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-     {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-     {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-     {
-      date: "2025-08-06",
-      id: "0cwHP5bnaMgPB8wV6vI4",
-      title: "Growing Business",
-      class: "資本家",
-      content:
-        "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
-    },
-  ];
+  // const data = [
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  //   {
+  //     title: "Foreign Market Insight",
+  //     date: "2025-08-06",
+  //     id: "21HXfUsvcuCJr8tD5l1O",
+  //     content:
+  //       "翻開2張出口卡\n選擇1張換掉現在的出口卡\n其餘卡丟棄\n然後,你可以做一次出售到國外的動作",
+  //     class: "資本家",
+  //   },
+  //   {
+  //     title: "Foreign Market Insight",
+  //     date: "2025-08-06",
+  //     id: "21HXfUsvcuCJr8tD5l1O",
+  //     content:
+  //       "翻開2張出口卡\n選擇1張換掉現在的出口卡\n其餘卡丟棄\n然後,你可以做一次出售到國外的動作",
+  //     class: "資本家",
+  //   },
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  //   {
+  //     date: "2025-08-06",
+  //     id: "0cwHP5bnaMgPB8wV6vI4",
+  //     title: "Growing Business",
+  //     class: "資本家",
+  //     content:
+  //       "免費建造一個倉庫\n放在一個指定資源下方\n然後建立一個公司\n如果該間公司生產的資源\n和該倉庫資源相同\n可以少付4元",
+  //   },
+  // ];
 
   // 第一張卡片初始位置
   const x = 15;
   const y = 10;
   // 卡片位置大小
   let card = { x, y, width: 60, height: 80 };
+
   const doc = new jsPDF();
-  //字型尺寸
-  doc.setFontSize(12);
 
   // 單張卡片
   const genCard = (x, y, obj) => {
@@ -121,22 +120,33 @@ export default function index() {
   };
 
   const generate = () => {
+    //字型尺寸
+    doc.setFontSize(12);
+
     // 中文字型
     doc.addFileToVFS("name-for-addFont-use", font);
     doc.addFont("name-for-addFont-use", "name-for-setFont-use", "normal");
     doc.setFont("name-for-setFont-use");
     // 資料迴圈產生所有卡片
-    for (let i = 0; i < data.length; i++) {
-      // console.log(i)
-      genCard(card.x, card.y, data[i]);
-      // 每3張換列
-      if (i % 3 == 2) {
+    for (let i = 0; i < rows.length; i++) {
+      console.log(i % 9);
+      genCard(card.x, card.y, rows[i]);
+      // 每3張換列(加上 i % 9 != 8 和 i % 9 == 8 條件區隔)
+      if (i % 3 == 2 && i % 9 != 8) {
         card.x = x;
         card.y += card.height;
+      } 
+      // 每9張換頁
+      else if (i % 9 == 8) {
+        card.x = x;
+        card.y = y;        
+        doc.addPage();
       } else {
-        card.x += card.width;
+        card.x += card.width;        
       }
     }
+
+    // doc.addPage();
 
     doc.save("table.pdf");
   };
