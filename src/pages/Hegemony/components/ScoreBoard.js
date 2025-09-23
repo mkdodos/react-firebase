85fea4563254bc05103d2880a031b0769e534efb
mkdodos <mkdodos@gmail.com>
2025-09-23 10:57:35 +0800

    abc

--- src/pages/Hegemony/components/ScoreBoard.js
+++ src/pages/Hegemony/components/ScoreBoard.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { da, faker } from "@faker-js/faker";
+import { v4 as uuidv4 } from "uuid";
 import {
   Table,
   TableHeader,
@@ -8,66 +11,51 @@ import {
   TableFooter,
 } from "semantic-ui-react";
 
-import { da, faker } from "@faker-js/faker";
-import "./scoreboard.css";
-
 export default function ScoreBoard() {
-  // ui celled unstackable table
-
-  // 單筆資料
-  const row = { date: "2025-09-22", player: "Mark", role: "中產", score: 100 };
-  const row1 = { date: "2025-09-22", player: "Dada", role: "勞工", score: 80 };
-
   // 多筆資料
   const data = [
     {
       date: "2025-09-22",
       player: "A",
-      role: "中產",
+      role: "middle",
       score: 10,
     },
     {
       date: "2025-09-23",
       player: "A",
-      role: "勞工",
+      role: "labor",
       score: 20,
     },
     {
       date: "2025-09-24",
       player: "A",
-      role: "政府",
+      role: "gov",
       score: 30,
     },
     {
       date: "2025-09-22",
       player: "B",
-      role: "勞工",
-      score: faker.number.int({ min: 1, max: 100 }),
+      role: "labor",
+      score: 100,
     },
     {
       date: "2025-09-22",
       player: "C",
-      role: "政府",
-      score: faker.number.int({ min: 1, max: 100 }),
+      role: "gov",
+      score: "20",
     },
     {
       date: "2025-09-22",
       player: "D",
-      role: "資本",
-      score: faker.number.int({ min: 1, max: 100 }),
+      role: "cap",
+      score: "30",
     },
   ];
-
-  // 某位玩家所有分數為一列
-  // 依玩家群組資料
-
-  //   const playerScores = Object.groupBy(data, ({ player }) => player);
-  //   console.log(playerScores.A);
 
   // 某日玩家分數記錄
   const scores = [
     {
-      player: "馬克",
+      player: "A",
       // 用特定鍵值代表角色分數
       gov: "100",
       middle: "80",
@@ -75,7 +63,7 @@ export default function ScoreBoard() {
       labor: "60",
     },
     {
-      player: "宜君",
+      player: "B",
       // 用特定鍵值代表角色分數
       gov: "10",
       middle: "18",
@@ -83,7 +71,7 @@ export default function ScoreBoard() {
       labor: "6",
     },
     {
-      player: "愷軒",
+      player: "C",
       // 用特定鍵值代表角色分數
       gov: "10",
       middle: "80",
@@ -91,7 +79,7 @@ export default function ScoreBoard() {
       labor: "6",
     },
     {
-      player: "欣妤",
+      player: "D",
       // 用特定鍵值代表角色分數
       gov: "10",
       middle: "8",
@@ -100,47 +88,102 @@ export default function ScoreBoard() {
     },
   ];
 
-  //   scores[0].total=500
+  // 日期群組
+  const dates = Object.groupBy(data, ({ date }) => date);
+  // console.log(Object.keys(dates));
 
-  const keys = Object.keys(scores[0]);
+  const getScore = (date, player, role) => {
+    // console.log(date);
+    // const score = data.find((obj) => obj.player == player && obj.role == role);
+    //  const score = data.find((obj) => obj.date == date);
+    const score = data.find(
+      (obj) => obj.date == date && obj.player == player && obj.role == role
+    );
 
-  let sum = 0;
-  keys.map((key) => {
-    if (key !== "player") {
-      sum += Number(scores[0][key]);
+    // console.log(score);
+
+    if (score) {
+      // console.log(score);
+      // console.log(date, player, role)
+      return score.score;
     }
-   
+
+    return 0;
+  };
+
+  const players = ["A", "B", "C", "D"];
+  // const players = ["馬克", "宜君", "愷軒", "欣妤"];
+  // const roles = ["勞工", "資本", "中產", "政府"];
+  const roles = ["gov", "cap", "middle", "labor"];
+
+  // 日期迴圈
+  Object.keys(dates).map((date) => {
+    // 用日期人員角色取得分數
+    // 人員迴圏
+    players.map((player) => {
+      const index = scores.findIndex((row) => row.player == player);
+      // console.log(index)
+      // console.log(scores[index]);
+
+      // 角色迴圈
+      roles.map((role) => {
+        console.log(player, role);
+        console.log(scores[index][role]);
+        scores[index][role] = getScore("2025-09-22", player, role);
+        // scores[index][role] = getScore("2025-09-22", "A", "middle");
+        // scores[index][role] = getScore("2025-09-22", "B", "labor");
+        // scores[index][role] = getScore("2025-09-22", "C", "gov");
+        // scores[index][role] = getScore("2025-09-22", "D", "cap");
+      });
+    });
   });
 
-   scores[0].total=sum
-   console.log(sum);
-
-//   console.log(Object.keys(scores[0]));
-
-  //   const roles = ["勞工", "資本", "中產", "政府"];
-  //   const players = ["馬克", "宜君", "愷軒", "欣妤"];
-
-  //   for (let i = 0; i < 4; i++) {
-  //     data.push({
-  //       date: "2025-09-22",
-  //       player: players[i],
-  //       role: roles[i],
-  //       score: faker.number.int({ min: 1, max: 100 }),
-  //     });
-  //   }
-
-  //   console.log(data)
+  console.log(scores);
 
   return (
     <div>
+      <Table celled unstackable>
+        <TableHeader>
+          <TableRow>
+            {Object.keys(data[0]).map((role) => {
+              return (
+                <TableHeaderCell key={uuidv4()} className={role}>
+                  {role}
+                </TableHeaderCell>
+              );
+            })}
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {data
+            .sort((a, b) => (a.date > b.date ? 1 : -1))
+            .map((obj) => {
+              return (
+                <TableRow key={uuidv4()}>
+                  {Object.keys(data[0]).map((key) => {
+                    return <TableCell key={uuidv4()}>{obj[key]}</TableCell>;
+                  })}
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
       <Table celled unstackable className="scoreboard">
         <TableHeader>
           <TableRow>
             <TableHeaderCell>姓名/角色</TableHeaderCell>
-            <TableHeaderCell className="labor">勞工</TableHeaderCell>
+            {roles.map((role) => {
+              return (
+                <TableHeaderCell key={uuidv4()} className={role}>
+                  {role}
+                </TableHeaderCell>
+              );
+            })}
+            {/* <TableHeaderCell className="labor">勞工</TableHeaderCell>
             <TableHeaderCell className="cap">資本</TableHeaderCell>
             <TableHeaderCell className="middle">中產</TableHeaderCell>
-            <TableHeaderCell className="gov">政府</TableHeaderCell>
+            <TableHeaderCell className="gov">政府</TableHeaderCell> */}
             <TableHeaderCell>合計</TableHeaderCell>
           </TableRow>
         </TableHeader>
@@ -148,62 +191,22 @@ export default function ScoreBoard() {
         <TableBody>
           {scores.map((obj) => {
             return (
-              <TableRow>
+              <TableRow key={uuidv4()}>
                 <TableCell>{obj.player}</TableCell>
-                <TableCell>{obj.labor}</TableCell>
+
+                {roles.map((role) => {
+                  return <TableCell key={uuidv4()}>{obj[role]}</TableCell>;
+                })}
+
+                {/* <TableCell>{obj.labor}</TableCell>
                 <TableCell>{obj.middle}</TableCell>
                 <TableCell>{obj.cap}</TableCell>
-                <TableCell>{obj.gov}</TableCell>
+                <TableCell>{obj.gov}</TableCell> */}
                 <TableCell>{obj.total}</TableCell>
               </TableRow>
             );
           })}
-
-          {/* <TableRow>
-            <TableCell>{scores[1].player}</TableCell>
-            <TableCell>{scores[1].labor}</TableCell>
-            <TableCell>{scores[1].middle}</TableCell>
-            <TableCell>{scores[1].cap}</TableCell>
-            <TableCell>{scores[1].gov}</TableCell>
-            <TableCell>Cell</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>{scores[1].player}</TableCell>
-            <TableCell>{scores[1].labor}</TableCell>
-            <TableCell>{scores[1].middle}</TableCell>
-            <TableCell>{scores[1].cap}</TableCell>
-            <TableCell>{scores[1].gov}</TableCell>
-            <TableCell>Cell</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>{scores[1].player}</TableCell>
-            <TableCell>{scores[1].labor}</TableCell>
-            <TableCell>{scores[1].middle}</TableCell>
-            <TableCell>{scores[1].cap}</TableCell>
-            <TableCell>{scores[1].gov}</TableCell>
-            <TableCell>{faker.number.int({ min: 1, max: 100 })}</TableCell>
-          </TableRow> */}
         </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableHeaderCell>合計</TableHeaderCell>
-            <TableHeaderCell>
-              {faker.number.int({ min: 1, max: 100 })}
-            </TableHeaderCell>
-            <TableHeaderCell>
-              {faker.number.int({ min: 1, max: 100 })}
-            </TableHeaderCell>
-            <TableHeaderCell>
-              {faker.number.int({ min: 1, max: 100 })}
-            </TableHeaderCell>
-            <TableHeaderCell>
-              {faker.number.int({ min: 1, max: 100 })}
-            </TableHeaderCell>
-            <TableHeaderCell>
-              {faker.number.int({ min: 100, max: 200 })}
-            </TableHeaderCell>
-          </TableRow>
-        </TableFooter>
       </Table>
     </div>
   );
